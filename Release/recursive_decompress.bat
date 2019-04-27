c625498bd812cdb7e036ada811a60f7482348b92
Dawid Sygocki <dawidsygocki1234@wp.pl>
2019-04-27 15:50:02 +0200

    Deleted no longer available option /s from script

--- Release/recursive_decompress.bat
+++ Release/recursive_decompress.bat
@@ -1,10 +1,10 @@
 @echo off
-rem USTAW PONI¯EJ ODPOWIEDNIE ŒCIE¯KI
-rem PRZESZUKIWANY FOLDER I FOLDER WYJŒCIOWY MO¯ESZ USTAWIÆ TE¯ W KONSOLI, PODAJ¥C JE JAKO ARGUMENTY
+rem USTAW PONIÂ¯EJ ODPOWIEDNIE Å’CIEÂ¯KI
+rem PRZESZUKIWANY FOLDER I FOLDER WYJÅ’CIOWY MOÂ¯ESZ USTAWIÃ† TEÂ¯ W KONSOLI, PODAJÂ¥C JE JAKO ARGUMENTY
 
-set depikczer=bezwzglêdna œcie¿ka do dePIKczer.exe
-set przeszukiwany_folder=bezwglêdna œcie¿ka przeszukiwanego folderu
-set folder_wyjsciowy=bezwglêdna œcie¿ka folderu wyjœciowego
+set depikczer=bezwzglÃªdna Å“cieÂ¿ka do dePIKczer.exe
+set przeszukiwany_folder=bezwglÃªdna Å“cieÂ¿ka przeszukiwanego folderu
+set folder_wyjsciowy=bezwglÃªdna Å“cieÂ¿ka folderu wyjÅ“ciowego
 
 
 
@@ -13,4 +13,4 @@ if not [%1]==[] (set przeszukiwany_folder=%1
 if not [%2]==[] (set folder_wyjsciowy=%2) else (set folder_wyjsciowy=)
 )
 if "%folder_wyjsciowy%"=="" (set opcja_out_dir=) else (set opcja_out_dir=/o \"%folder_wyjsciowy%\")
-forfiles /p %przeszukiwany_folder% /m *.img /s /c "%depikczer% /s %opcja_out_dir% @path"
+forfiles /p %przeszukiwany_folder% /m *.img /s /c "%depikczer% %opcja_out_dir% @path"
